8c7dc1e4f29145bebc47d8dca29b0cfaaf4d528c
chunta <imchuntachen@gmail.com>
2024-09-04 23:34:17 +0800

    KAN-3 TestAgain (#26)
    * text
    
    * KAN-3 s
    
    ---------
    
    Co-authored-by: rex <rex>

--- MainApp/MainApp/ContentView.swift
+++ MainApp/MainApp/ContentView.swift
@@ -4,7 +4,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("1")
-            Text("1")
+            Text("111")
+            Text("1111")
         }
     }
 }
