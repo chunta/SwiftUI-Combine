bf3b546ac66209ee573bbf4a83d82d01b81547c8
chunta <imchuntachen@gmail.com>
2024-09-04 23:14:29 +0800

    KAN-2 add vstack (#25)
    Co-authored-by: rex <rex>

--- MainApp/MainApp/ContentView.swift
+++ MainApp/MainApp/ContentView.swift
@@ -2,8 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, the big world!")
-            .padding()
+        VStack {
+            Text("1")
+            Text("1")
+        }
     }
 }
 
