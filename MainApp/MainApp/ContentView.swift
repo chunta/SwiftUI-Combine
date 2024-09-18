421ff4c71b63dc0cdecc78587867d7aa302f2cd9
rex <rex>
2024-09-18 13:52:03 +0800

    have DEV, UAT, PRO

--- MainApp/MainApp/ContentView.swift
+++ MainApp/MainApp/ContentView.swift
@@ -4,18 +4,26 @@ struct ContentView: View {
     var bundleVersion: String {
         return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown Version"
     }
-
+    
     var bundleBuild: String {
         return Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown Build"
     }
-
+    
     var body: some View {
         VStack {
             Text("1")
             Text("111")
             Text("1111")
-            Text("Bundle Version: \(bundleVersion).\(bundleBuild)")
+#if DEV
+            Text("DEBUG Mode - Bundle Version: \(bundleVersion).\(bundleBuild)")
                 .padding()
+#elseif UAT
+            Text("UAT Mode - Bundle Version: \(bundleVersion).\(bundleBuild)")
+                .padding()
+#else
+            Text("PRO Mode - Bundle Version: \(bundleVersion).\(bundleBuild)")
+                .padding()
+#endif
         }
     }
 }
