717f23500b7a7f19d8a2e033209a9495a6589ac2
rex <rex>
2024-09-18 12:19:44 +0800

    Display BundleVersion BundleNumber

--- MainApp/MainApp/ContentView.swift
+++ MainApp/MainApp/ContentView.swift
@@ -1,11 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    var bundleVersion: String {
+        return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown Version"
+    }
+
+    var bundleBuild: String {
+        return Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown Build"
+    }
+
     var body: some View {
         VStack {
             Text("1")
             Text("111")
             Text("1111")
+            Text("Bundle Version: \(bundleVersion).\(bundleBuild)")
+                .padding()
         }
     }
 }
